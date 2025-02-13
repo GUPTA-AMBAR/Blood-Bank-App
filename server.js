2deb990a0dfe9eeec3830f6915b2f140a027cd9c
GUPTA-AMBAR <ambargupta1290@gamil.com>
2025-02-13 23:07:01 +0530

    static file build

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const cors = require('cors');
 const connectDtatabase = require('./db/connectDataBase.js');
+const path = require("path");
 dotenv.config();
 
 const app = express();
+
+// Serve static frontend files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
 
 // Middleware for logging requests
 app.use(express.json());
@@ -17,15 +26,15 @@ app.use(cors({
 
 connectDtatabase();
 
-const PORT = process.env.PORT|| 8080;
+const PORT = process.env.PORT || 8080;
 
-app.use('/api/v1/auth',require("./routes/authRoutes.js"));
+app.use('/api/v1/auth', require("./routes/authRoutes.js"));
 app.use('/api/v1/inventory', require("./routes/inventoryRoutes.js"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes.js"));
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running in ${process.env.DEV_MODE} on port ${process.env.PORT}`
         .bgBlue.white
     );
-    
+
 })
